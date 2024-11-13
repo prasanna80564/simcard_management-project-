00d7eca517848a57c8ce8307e4d119e41ef54b27
prasanna80564 <20039859@mydbs.ie>
2024-11-13 21:53:03 +0000

    delete customer

--- public/edit-customer.js
+++ public/edit-customer.js
@@ -1,4 +1,4 @@
-const { MongoCryptCreateDataKeyError } = require("mongodb");
+
 
 function fetchCustomerDetails() {
     const urlParmas = new URLSearchParams(window.location.search);
@@ -18,3 +18,44 @@ function fetchCustomerDetails() {
 }
 
 //edit customer details
+document.getElementById('editCustomerForm').addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const customerId = document.getElementById('customerId').value;
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+    const updatedCustomer = {name, email, phone};
+
+    fetch(`http://localhost:3000/customers/${customerId}`,{
+        method : 'PUT',
+        body: JSON.stringify(updatedCustomer),
+        headers: {'Content-Type':'application/json'}
+
+    })
+    .then(response => response.json())
+    .then(()=> {
+        alert('customer is updated successfully!');
+        window.location.href = 'index.html';
+
+    })
+    .catch(error => ('error updating customer:',error));
+
+});
+
+//delete customer
+
+function deleteCustomer(id) {
+    fetch(`http://localhost:3000/customers/${id}`, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(() => {
+        alert('Customer successfully deleted!');
+        window.location.href = 'index.html';
+    })
+    .catch(error => console.error('Error deleting customer:', error));
+}
+
+document.addEventListener('DOMContentLoaded',fetchCustomerDetails);
