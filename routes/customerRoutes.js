988bd3a62c8e6bdd6a7b9816d911e4da70ed3164
prasanna80564 <20039859@mydbs.ie>
2024-11-14 14:10:30 +0000

    routes for adding customers

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -6,8 +6,25 @@ const Customer = require('../models/Customer');
 
 router.get('/', async (req,res) => {
     try {
-        const customers = await Customer.find();
-        res.json(customers);
+        const customer = await Customer.find();
+        res.json(customer);
+    }
+    catch(err) {
+        res.status(500).json({message: err.message});
+    }
+});
+
+// add customers
+
+router.post('/add', async (req,res) => {
+    const customer = new Customer({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone
+
+    });
+    try{
+        const newCustomer = await customer.save();
     }
     catch(err) {
         res.status(500).json({message: err.message});
