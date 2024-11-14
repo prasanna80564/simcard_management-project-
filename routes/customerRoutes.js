3781b4c4f8c38852e73a804fd50b4487a419c976
prasanna80564 <20039859@mydbs.ie>
2024-11-14 14:14:03 +0000

    server.js is updated

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -31,4 +31,6 @@ router.post('/add', async (req,res) => {
     }
 });
 
+
+
 module.exports = router;//
--- server.js
+++ server.js
@@ -3,32 +3,32 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const customerRoutes = require('./routes/customerRoutes');
-
+const customerRoutes = require('./routes/customerRoutes'); 
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public')));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(cors());
 app.use(bodyParser.json());
+
 
 app.use('/customers', customerRoutes);
 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+}); 
 
 
 mongoose.connect('mongodb://localhost:27017/telecom')
     .then(() => console.log('connected to MongoDB'))
-    .catch(err => console.log('failed to connect:',err));
+    .catch(err => console.log('failed to connect:', err));
 
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () =>{
-    console.log(`server is running on port ${PORT}`);
-}
-);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
