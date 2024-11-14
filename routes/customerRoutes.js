c807dfaca016d99618c50f45cd5c139e9e037327
prasanna80564 <20039859@mydbs.ie>
2024-11-14 13:26:50 +0000

    routes for customers

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -2,3 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Customer = require('../models/Customer');
 
+// all customers
+
+router.get('/', async (req,res) => {
+    try {
+        const customers = await Customer.find();
+        res.json(customers);
+    }
+    catch(err) {
+        res.status(500).json({message: err.message});
+    }
+});
