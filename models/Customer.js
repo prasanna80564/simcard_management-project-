1c133c3d456c85df4ebf7e5a2b265185188c6f15
prasanna80564 <20039859@mydbs.ie>
2024-11-20 13:47:41 +0000

    models updated

--- models/Customer.js
+++ models/Customer.js
@@ -4,13 +4,12 @@
 const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    email: {type:String,required: true},
-    phone: {type:String , required:true}
-
-
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true }
 });
 
-const Customer = mongoose.model('Customer',customerSchema);
+const Customer = mongoose.model('Customer', customerSchema);
 
 module.exports = Customer;
+
