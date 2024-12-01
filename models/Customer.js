b4e2ca3d4abf31333b4b9c0063543b88fc653744
prasanna80564 <20039859@mydbs.ie>
2024-12-01 14:34:19 +0000

    customer scheme is updated

--- models/Customer.js
+++ models/Customer.js
@@ -6,7 +6,12 @@ const mongoose = require('mongoose');
 const customerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: String, required: true }
+  simcard: {
+    phoneNumber: { type: String, required: true},
+    plan: {type: String, enum: ['Basic','Gold','Platinum'], required: true},
+    isActive: {type: Boolean, default: false}
+
+  }
 });
 
 const Customer = mongoose.model('Customer', customerSchema);
