0f857a94a54d243714bace3dbf6e6be430cd1eca
prasanna80564 <20039859@mydbs.ie>
2024-12-07 13:44:28 +0000

    get customers in customers routes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,3 +3,15 @@ const router = express.Router();
 const Customer = require('../models/Customer');
 
 
+router.get('/', async (req,res) => {
+  try {
+    const customers = await Customer.find();
+    res.json(customers);
+  }
+  catch (err) {
+    res.status(500).json({ message: err.message});
+  }
+
+
+});
+
