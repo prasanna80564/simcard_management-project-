632914e2963f29ee8bf3784fb7fc5bb90705a969
prasanna80564 <20039859@mydbs.ie>
2024-12-09 21:34:15 +0000

    server.js is updated

--- server.js
+++ server.js
@@ -1,38 +1,26 @@
-//main server 
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const customerRoutes = require('./routes/customerRoutes'); 
+const customerRoutes = require('./routes/customerRoutes');
 
 const app = express();
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use(cors());
 app.use(bodyParser.json());
-
-
 app.use('/customers', customerRoutes);
 
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-}); 
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-
-mongoose.connect('mongodb+srv://admin:prasanna2703@cluster1.wzuur.mongodb.net/telecom?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error('Failed to connect:', err));
+mongoose.connect('mongodb://localhost:27017/telecom', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
