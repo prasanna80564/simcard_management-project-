fc05e0022127b7b9a7f5b3095ea2d8ca03429601
prasanna80564 <20039859@mydbs.ie>
2024-12-09 19:18:15 +0000

    test.js

--- tests/customer.test.js
+++ tests/customer.test.js
@@ -1,1 +1,9 @@
-const request = require('supertest';)
+const request = require('supertest');
+const app = require('../server');
+const Customer = require('../models/customer');
+
+describe('Cusomer API', () => {
+    beforeALL(async () => {
+        await Customer.deleteMany({});
+    });
+});
