44f590beb1eb70ea0f978fbe034405cdc07ce7e5
prasanna80564 <20039859@mydbs.ie>
2024-12-11 20:35:37 +0000

    before all test

--- tests/customer.test.js
+++ tests/customer.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
-const app = require('../server');
-const Customer = require('../models/customer');
+const mongoose = require('mongoose');
+const app = require('../server');  
+const Customer = require('../models/Customer');
 
-describe('Cusomer API', () => {
-    beforeALL(async () => {
-        await Customer.deleteMany({});
-    });
-});
+beforeAll(async () => {
+    if (mongoose.connection.readyState === 0) { // 0 = disconnected
+      const mongoURI = 'mongodb://localhost:27017/telecom_test';  // Use a test database
+      await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    }
+  });
 
